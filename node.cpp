c25808d9fcb925d76704f8b9b9b335653cbd5646
Tim Siebels <tim_siebels_aurich@yahoo.de>
2015-12-02 18:31:53 +0100

    Move Node to .cpp
    this was temporary, but existed for way too long

--- node.cpp
+++ node.cpp
@@ -1,1 +1,76 @@
 #include "node.h"
+
+Node::Node(QGraphicsScene* scene, const std::string& content, const QRectF& rect /* = QRectF(0, 0, 100, 30)*/)
+  : QGraphicsRectItem(rect),
+    m_scene(scene) {
+  setFlag(QGraphicsItem::ItemIsMovable);
+  setFlag(QGraphicsItem::ItemSendsScenePositionChanges);
+
+  m_text = new QGraphicsTextItem(this);
+  m_text->setPlainText(content.c_str());
+  m_text->setPos(rect.topLeft());
+  m_scene->addItem(m_text);
+  m_scene->addItem(this);
+}
+
+
+void Node::addChildNode(Node* node) {
+  auto line = m_scene->addLine(0,0,0,0);
+  addLine(line, true);
+  node->addLine(line, false);
+  node->setParent(this);
+  m_nodes.append(node);
+}
+
+void Node::addLine(QGraphicsLineItem *line, bool isPoint1) {
+  m_lines.push_back(QPair<QGraphicsLineItem*, bool>(line, isPoint1));
+}
+
+QVariant Node::itemChange(GraphicsItemChange change, const QVariant& value)
+{
+  if (change == ItemPositionChange && scene()) {
+    // value is the new position.
+    QPointF newPos = value.toPointF();
+
+    moveLines(newPos);
+    m_text->setPos(newPos);
+    if (getText() != "/")
+      arrange();
+  }
+  return QGraphicsItem::itemChange(change, value);
+}
+
+void Node::moveLines(QPointF newPos) {
+  // Converts the elipse position (top-left)
+  // to its center position
+  int xOffset = rect().x() + rect().width()/2;
+
+  for (auto& line_pair : m_lines.toStdVector()) {
+    auto is_p1 = line_pair.second;
+    auto line = line_pair.first;
+
+    QPointF other_pnt = is_p1 ? line->line().p2() : line->line().p1();
+
+    QPointF newLinePos;
+    if (other_pnt.y() > newPos.y()) {
+      newLinePos = QPointF(newPos.x() + xOffset, newPos.y() + rect().height());
+    } else {
+      newLinePos = QPointF(newPos.x() + xOffset, newPos.y());
+    }
+
+    // Move the required point of the line to the center of the elipse
+    QPointF p1 = is_p1 ? newLinePos : line->line().p1();
+    QPointF p2 = is_p1 ? line->line().p2() : newLinePos;
+
+    line->setLine(QLineF(p1, p2));
+  }
+}
+
+std::string Node::getText() const {
+  if (m_text == nullptr) return "";
+  return m_text->toPlainText().toStdString();
+}
+
+void Node::setParent(Node* parent) {
+  m_parent = parent;
+}
--- node.h
+++ node.h
@@ -10,72 +10,15 @@
 class Node : public QGraphicsRectItem
 {
 public:
-  explicit Node(QGraphicsScene* scene, const std::string& content, const QRectF& rect = QRectF(0, 0, 100, 30))
-    : QGraphicsRectItem(rect),
-      m_scene(scene) {
-    setFlag(QGraphicsItem::ItemIsMovable);
-    setFlag(QGraphicsItem::ItemSendsScenePositionChanges);
+  explicit Node(QGraphicsScene* scene, const std::string& content, const QRectF& rect = QRectF(0, 0, 100, 30));
 
-    m_text = new QGraphicsTextItem(this);
-    m_text->setPlainText(content.c_str());
-    m_text->setPos(rect.topLeft());
-    m_scene->addItem(m_text);
-    m_scene->addItem(this);
-  }
+  void addChildNode(Node* node);
+  void addLine(QGraphicsLineItem *line, bool isPoint1);
+  QVariant itemChange(GraphicsItemChange change, const QVariant& value) override;
+  void moveLines(QPointF newPos);
 
 
-  void addChildNode(Node* node) {
-    auto line = m_scene->addLine(0,0,0,0);
-    addLine(line, true);
-    node->addLine(line, false);
-    node->setParent(this);
-    m_nodes.append(node);
-  }
-
-  void addLine(QGraphicsLineItem *line, bool isPoint1) {
-    m_lines.push_back(QPair<QGraphicsLineItem*, bool>(line, isPoint1));
-  }
-
-  QVariant itemChange(GraphicsItemChange change, const QVariant& value)
-  {
-    if (change == ItemPositionChange && scene()) {
-      // value is the new position.
-      QPointF newPos = value.toPointF();
-
-      moveLines(newPos);
-      m_text->setPos(newPos);
-      if (getText() != "/")
-        arrange();
-    }
-    return QGraphicsItem::itemChange(change, value);
-  }
-
-  void moveLines(QPointF newPos) {
-    // Converts the elipse position (top-left)
-    // to its center position
-    int xOffset = rect().x() + rect().width()/2;
-
-    for (auto& line_pair : m_lines.toStdVector()) {
-      auto is_p1 = line_pair.second;
-      auto line = line_pair.first;
-
-      QPointF other_pnt = is_p1 ? line->line().p2() : line->line().p1();
-
-      QPointF newLinePos;
-      if (other_pnt.y() > newPos.y()) {
-        newLinePos = QPointF(newPos.x() + xOffset, newPos.y() + rect().height());
-      } else {
-        newLinePos = QPointF(newPos.x() + xOffset, newPos.y());
-      }
-
-      // Move the required point of the line to the center of the elipse
-      QPointF p1 = is_p1 ? newLinePos : line->line().p1();
-      QPointF p2 = is_p1 ? line->line().p2() : newLinePos;
-
-      line->setLine(QLineF(p1, p2));
-    }
-  }
-
+  // This is here, because it fails to link, because <true> isn't generated
   template <bool isRoot = false>
   void arrange() {
     constexpr float TAU = 2 * 3.1415926;
@@ -106,15 +49,8 @@ public:
     }
   }
 
-  std::string getText() const {
-    if (m_text == nullptr) return "";
-    return m_text->toPlainText().toStdString();
-  }
-
-  void setParent(Node* parent) {
-    m_parent = parent;
-  }
-
+  std::string getText() const;
+  void setParent(Node* parent);
 private:
   // <Line, is_p1>
   QVector<QPair<QGraphicsLineItem*, bool>> m_lines;
