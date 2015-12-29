f49615a3df069f835c053b17317b674dbb6d869e
Tim Siebels <tim_siebels_aurich@yahoo.de>
2015-12-29 15:41:18 +0100

    Make Finding libgitcpp generic
    not perfect, but working well

---
+++ FindLibgitcpp.cmake
@@ -0,0 +1,14 @@
+find_path(
+  LIBGITCPP_INCLUDE_DIR NAMES Repository.h Commit.h
+  PATH_SUFFIXES libgitcpp/include
+  PATHS
+  /home/tim/Projects/
+  /var/lib
+  /opt
+  /opt/local
+)
+
+message("Libgitcpp include dir: " ${LIBGITCPP_INCLUDE_DIR})
+include_directories(${LIBGITCPP_INCLUDE_DIR})
+
+mark_as_advanced(LIBGITCPP_INCLUDE_DIR)
